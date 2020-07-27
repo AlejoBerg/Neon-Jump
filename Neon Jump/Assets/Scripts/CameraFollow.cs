616ce7967081014c11aedc25ce2758213eb75c86
Alejo B <alejo.110601@gmail.com>
2020-07-27 20:14:00 -0300

    Movimiento Basico
    - Movimiento Player (WIP)
    - Movimiento Camara (WIP)
    - Colision mediante layers con plataformas

---
+++ Neon Jump/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target = null;
+    [SerializeField] private Vector3 cameraOffset;
+    private Vector3 referenceVelocity = Vector3.zero;
+    private float delayAmount = 0.05f;
+    private Vector3 finalPosition;
+
+    private void LateUpdate()
+    {
+        finalPosition = target.position + cameraOffset;
+        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, finalPosition, ref referenceVelocity, delayAmount);
+        //Vector3 smoothedPosition = Vector3.Slerp(transform.position, finalPosition, delayAmount);
+        transform.position = smoothedPosition;
+        transform.LookAt(target);
+    }
+}
---
+++ Neon Jump/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 0;
+    [SerializeField] private float jumpForwardMovementSpeed = 2; 
+    private Rigidbody myRigidbody = null;
+    private float jumpForce = 5;
+    private bool isGrounded = true;
+
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+        PlayerJump();
+    }
+
+    private void PlayerMovement()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 playerMovement = new Vector3(horizontal, 0, 0).normalized ;
+        transform.Translate(playerMovement * speed * Time.deltaTime);
+    }
+
+    private void PlayerJump()
+    {
+        if (isGrounded == true)
+        {
+            isGrounded = false;
+            myRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            myRigidbody.AddForce(Vector3.forward * jumpForwardMovementSpeed, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.layer == 8)
+        {
+            isGrounded = true;
+        }
+    }
+}
