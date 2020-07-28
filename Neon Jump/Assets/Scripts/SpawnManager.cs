6ed4113416f5c2190495899ac53b70fa64ff87c1
RomanVitolo <68485569+RomanVitolo@users.noreply.github.com>
2020-07-28 13:34:28 -0300

    Revert "Revert "Pool en proceso""
    This reverts commit 6927031d8708f0f6b6e9d7a788b5cfd44a1e7fb0.

---
+++ Neon Jump/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    SpawnPlatforms spawnPlatforms;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPlatforms = GetComponent<SpawnPlatforms>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnTriggerEntered()
+    {
+        spawnPlatforms.MovePlatform();
+    }
+}
