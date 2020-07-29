36b553b3bbdb0ba7adf6478bb6f6072a93b34ff3
Alejo B <alejo.110601@gmail.com>
2020-07-29 01:55:17 -0300

    Salto del Personaje WIP

--- Neon Jump/Assets/Scripts/PlayerController.cs
+++ Neon Jump/Assets/Scripts/PlayerController.cs
@@ -4,22 +4,45 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject [] platformsReference;
+    private Queue<GameObject> platformsQueue;
+    private GameObject nextPlatform;
+
     [SerializeField] private float jumpForwardMovementSpeed = 2; //Cuanto se va a mover para adelante cuando salta
-    [SerializeField] private float jumpForce = 5; // Cuan alto va a saltar
+    [SerializeField] private float jumpForce = 4; // Cuan alto va a saltar
 
     private Rigidbody myRigidbody = null;
     private bool isGrounded = true;
 
     private void Start()
     {
+        platformsQueue = new Queue<GameObject>();
         myRigidbody = GetComponent<Rigidbody>();
+        
+        foreach (var platform in platformsReference)
+        {
+            platformsQueue.Enqueue(platform);
+        }
+
+        nextPlatform = platformsQueue.Peek();
+
     }
 
     private void Update()
     {
+        PlayerJump();
         PlayerMovement();
         PlayerMovementKeyboard();//Solo para test
-        PlayerJump();
+
+        if (transform.position.z < nextPlatform.transform.position.z)
+        {
+            Vector3 currentVelocity = new Vector3(0,0,5);
+            Vector3 dampedPosition = Vector3.SmoothDamp(transform.position, nextPlatform.transform.position, ref currentVelocity, 0.3f);
+            transform.position = dampedPosition;
+            //float smoothTime = 5;
+            //var dampedPos = Mathf.SmoothDamp(transform.position.z, nextPlatform.transform.position.z, ref smoothTime, 2f);
+            //transform.position = new Vector3(transform.position.x, transform.position.y, dampedPos);
+        }
     }
 
     private void PlayerMovement()
@@ -39,8 +62,13 @@ public class PlayerController : MonoBehaviour
         if (isGrounded == true)
         {
             isGrounded = false;
+            nextPlatform = platformsQueue.Peek();
+            var diff = Mathf.Abs(Mathf.Abs(this.transform.position.z) - Mathf.Abs(nextPlatform.transform.localPosition.z));
+
             myRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            myRigidbody.AddForce(Vector3.forward * jumpForwardMovementSpeed, ForceMode.Impulse);
+
+            //myRigidbody.velocity *= 0f;
+            platformsQueue.Dequeue();
         }
     }
 
