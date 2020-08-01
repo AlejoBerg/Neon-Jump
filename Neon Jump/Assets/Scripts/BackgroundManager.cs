189a774a825127831f53c836fa03f69f0be05d71
Alejo B <alejo.110601@gmail.com>
2020-08-01 16:21:30 -0300

    Agregados Visuales Basicos
    Agregue el obj de la plataforma.
    Retoques al fondo
    Background Manager WIP

---
+++ Neon Jump/Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+    [SerializeField] private Material sky;
+    private Color initialColor;
+    private Color endColor;
+
+    void Start()
+    {
+        initialColor = new Color(0, 0, 0);
+        endColor = new Color(0, 0, 0);
+        RenderSettings.skybox.SetFloat("_Exposure", 6);
+        RenderSettings.skybox.SetColor("_Tint", Color.white);
+        print($"el color actual del cielo es {RenderSettings.skybox}");
+    }
+
+    void Update()
+    {
+        //RenderSettings.skybox.SetColor("_Tint", Color.Lerp(initialColor, endColor, 0.2f));
+    }
+}
